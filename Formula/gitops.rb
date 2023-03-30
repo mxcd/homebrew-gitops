1c4e58335482516c81fd069db2d1d37b05ace8b8
Max <max.partenfelder@wilde-it.com>
2023-03-31 01:57:32 +0200

    feat: bumping to beta 2

--- Formula/gitops.rb
+++ Formula/gitops.rb
@@ -1,8 +1,8 @@
 class Gitops < Formula
   desc "CLI tool for performing gitops tasks"
   homepage "https://github.com/mxcd/gitops-cli"
-  url "https://github.com/mxcd/gitops-cli/archive/2.0.0-beta-1.tar.gz"
-  sha256 "5f656ef701544241fb3142a3aad540eec84ef84639203ee3a9b78b5d0dc78ab9"
+  url "https://github.com/mxcd/gitops-cli/archive/2.0.0-beta-2.tar.gz"
+  sha256 "a99bfc6769eb6e94ed9fb1fe4223f4567e896f85c836c34ce37d5b9eb49704ca"
   license "MIT"
   depends_on "go" => :build
 
