379e0af24808ba76595ec7b10f4aead688ef784f
Max <max.partenfelder@wilde-it.com>
2023-03-31 01:43:33 +0200

    feat: adding gitops formula

---
+++ Formula/gitops.rb
@@ -0,0 +1,20 @@
+class Gitops < Formula
+  desc "CLI tool for performing gitops tasks"
+  homepage "https://github.com/mxcd/gitops-cli"
+  url "https://github.com/mxcd/gitops-cli/archive/2.0.0-beta-1.tar.gz"
+  sha256 "5f656ef701544241fb3142a3aad540eec84ef84639203ee3a9b78b5d0dc78ab9"
+  license "MIT"
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    path = buildpath/"src/github.com/mxcd/gitops-cli"
+    cd path do
+      system "go", "build", "-o", "#{bin}/gitops"
+    end
+  end
+
+  test do
+    shell_output("#{bin}/gitops", "-h")
+  end
+end
