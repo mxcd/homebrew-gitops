19324d46292b61bd97fd7ece525cf7ca0e48635f
Max Partenfelder <max.partenfelder@wilde-it.com>
2023-04-09 11:59:30 +0000

    feat: updating to version 2.0.0-rc1

--- Formula/gitops.rb
+++ Formula/gitops.rb
@@ -1,8 +1,8 @@
 class Gitops < Formula
   desc "CLI tool for performing gitops tasks"
   homepage "https://github.com/mxcd/gitops-cli"
-  url "https://github.com/mxcd/gitops-cli/archive/2.0.0-beta-7.tar.gz"
-  sha256 "41c91569b64dd5a8b3db4f961f0ff399044b2243a2a3eb455a7907cb0cf1b1c4"
+  url "https://github.com/mxcd/gitops-cli/archive/2.0.0-rc1.tar.gz"
+  sha256 "f1201b343bc7011afb5c4dc2b8aef8ef3be0722af949cc73822d509533836149"
   license "MIT"
   depends_on "go" => :build
 
