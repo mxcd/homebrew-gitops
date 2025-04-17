8a4271f670c69a7dd6561922a79a2c7d751f96d4
Max Partenfelder <max.partenfelder@wilde-it.com>
2025-04-17 17:51:36 +0000

    feat: updating to version 2.2.3

--- Formula/gitops.rb
+++ Formula/gitops.rb
@@ -1,8 +1,8 @@
 class Gitops < Formula
   desc "CLI tool for performing gitops tasks"
   homepage "https://github.com/mxcd/gitops-cli"
-  url "https://github.com/mxcd/gitops-cli/archive/2.2.2.tar.gz"
-  sha256 "cff720d0013fbab33af64cddab52865cf075b98abf206188936ee8b2268fc165"
+  url "https://github.com/mxcd/gitops-cli/archive/2.2.3.tar.gz"
+  sha256 "e65b70a5184fe1d77c12ebacd8b24445caba566deae9f15d7749eb7a98592d20"
   license "MIT"
   depends_on "go" => :build
 
@@ -10,7 +10,7 @@ class Gitops < Formula
     # ENV["GOPATH"] = buildpath
     path = buildpath/"cmd/gitops"
     cd path do
-      system "go", "build", "-o", "#{bin}/gitops"
+      system "go", "build", "-ldflags=\"-s -w -X 'main.version=2.2.3'\"" "-o", "#{bin}/gitops"
     end
   end
 
